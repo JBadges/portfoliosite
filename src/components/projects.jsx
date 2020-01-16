5519e4bfe31bea0785d1c2f2caeabae86abba156
jbadges <jackson.brajer@gmail.com>
2020-01-16 17:58:20 -0500

    ui

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,15 +4,86 @@ import Boids from "./boids";
 import FRC2019 from "./frc2019";
 import Navigation from "./navigation";
 
+import Particles from "react-particles-js";
+
 class Projects extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Navigation />
+
         <div className="scroll-container">
+          <Particles
+            canvasClassName="test"
+            params={{
+              fps_limit: 120,
+              particles: {
+                number: {
+                  value: 200,
+                  density: {
+                    enable: true
+                  }
+                },
+                line_linked: {
+                  enable: true,
+                  distance: 100,
+                  opacity: 0.3
+                },
+                move: {
+                  speed: 1.4
+                },
+                opacity: {
+                  anim: {
+                    enable: true,
+                    opacity_min: 0.05,
+                    speed: 2,
+                    sync: false
+                  },
+                  value: 0.4
+                }
+              },
+              polygon: {
+                enable: false,
+                scale: 0.75,
+                type: "inline",
+                move: {
+                  radius: 10
+                },
+                url: "/assets/smalldeer.svg",
+                inline: {
+                  arrangement: "equidistant"
+                },
+                draw: {
+                  enable: true,
+                  stroke: {
+                    color: "rgba(255, 255, 255, .2)"
+                  }
+                }
+              },
+              retina_detect: false,
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "bubble"
+                  },
+                  onclick: {
+                    enable: true,
+                    mode: "repulse"
+                  }
+                },
+                modes: {
+                  bubble: {
+                    size: 6,
+                    distance: 125
+                  }
+                }
+              }
+            }}
+          />
+          <FRC2019 />
           <PathFollowing />
           <Boids />
-          <FRC2019 />
         </div>
       </React.Fragment>
     );
