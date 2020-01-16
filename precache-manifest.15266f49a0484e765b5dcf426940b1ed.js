6b7747d6eb50ad0d82d20a0665dcd14243ae7ea3
jbadges <jackson.brajer@gmail.com>
2020-01-16 15:24:28 -0500

    Updates

---
+++ precache-manifest.15266f49a0484e765b5dcf426940b1ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aae5d5b10882ba9c932add83e84ce21",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "f0f0711653df48221d8f",
+    "url": "/portfoliosite/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "912549e9cad1a3182600",
+    "url": "/portfoliosite/static/css/main.6a356892.chunk.css"
+  },
+  {
+    "revision": "f0f0711653df48221d8f",
+    "url": "/portfoliosite/static/js/2.72d92d1d.chunk.js"
+  },
+  {
+    "revision": "9f007689e4e6f1520dd8392af203676e",
+    "url": "/portfoliosite/static/js/2.72d92d1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "912549e9cad1a3182600",
+    "url": "/portfoliosite/static/js/main.e8052602.chunk.js"
+  },
+  {
+    "revision": "741c980c07fed2098e03",
+    "url": "/portfoliosite/static/js/runtime-main.77723c7d.js"
+  }
+]);
